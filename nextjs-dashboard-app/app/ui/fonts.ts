3aba737e59c2b1549727c91722eb5840f187e21b
orondon <oscar.rondon.c@gmail.com>
2023-11-03 19:09:23 -0300

    nextjs-dashboard-app [move as independent project]

--- nextjs-dashboard-app/app/ui/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Lusitana, Montserrat } from 'next/font/google'
-
-export const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '500', '600', '700'] })
-
-export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
